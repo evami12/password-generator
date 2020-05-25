1c757b9fa7ecd22baedfe8b7388faaa148fd9044
Evelyn <kuo.e@rangiruru.school.nz>
2020-05-25 15:01:35 +1200

    added generate password button that kinda works
    I tested expected inputs of checkboxes lowercase, numbers, uppercase, and special characters.
    
    I also tested inputs of the password length from boundary numbers 1 - 15.
    
    I tested an invalid input of no checkboxes selected and this made it crash so I will need to fix this issue.
    
    It is also randomly selecting from the checkboxes people choose so the random password may not contain every type of character they want.

--- src/index.svelte
+++ src/index.svelte
@@ -1,29 +1,58 @@
 <script>
+  let lowercase = false;
+  let numbers = false;
+  let specialCharacters = false;
+  let uppercase = false;
 
-function makePassword(){
-}
+  let passwordLength = 4;
+  let password = "";
 
-
-
+  function makePassword() {
+    while (password.length < passwordLength) {
+        let randomSelection = Math.floor(Math.random() * 4 + 1);
+      if (randomSelection === 1 && lowercase) {
+        let randomNum = Math.floor(Math.random() * 26 + 97);
+        password += String.fromCharCode(randomNum);
+      }
+      if (randomSelection === 2 && numbers) {
+        let randomNum = Math.floor(Math.random() * 10 + 48);
+        password += String.fromCharCode(randomNum);
+      }
+      if (randomSelection === 3 && specialCharacters) {
+        let randomNum = Math.floor(Math.random() * 11 + 33);
+        password += String.fromCharCode(randomNum);
+      }
+      if (randomSelection === 4 && uppercase) {
+        let randomNum = Math.floor(Math.random() * 26 + 65);
+        password += String.fromCharCode(randomNum);
+      }
+    } 
+  }
 </script>
 
 <style>
+
 </style>
 
-
 <section class="section content">
-<h1>Password Generator!</h1>
+  <h1>Password Generator!</h1>
+  Password Length: {passwordLength}
+  <input
+    class="slider"
+    type="range"
+    min="1"
+    max="15"
+    bind:value={passwordLength} />
 
-Password Length:
-<input type=number>
-
-
-<p>I would like:</p>
-<input type=checkbox id="input1"> Letters
-<input type=checkbox> Numbers
-<input type=checkbox> Special Characters
-
-
-<button on:click={makePassword}>Generate Password!</button>
-
+  <p>I would like:</p>
+  <input type="checkbox" bind:value={lowercase} />
+  Lowercase Letters
+  <input type="checkbox" bind:value={numbers} />
+  Numbers
+  <input type="checkbox" bind:value={specialCharacters} />
+  Special Characters
+  <input type="checkbox" bind:value={uppercase} />
+  Uppercase Letters
+  <button on:click={makePassword}>Generate Password!</button>
+  {password}
 </section>
