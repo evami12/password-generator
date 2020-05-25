e7068bfdd6ab024540a5fbcac056606918173b7d
Evelyn <kuo.e@rangiruru.school.nz>
2020-05-25 15:14:38 +1200

    fixed earlier errors with invalid inputs
    Lowercase letters is now automatically checked so users will less likely make anerror of not checking a box. If no boxes are checked, it will notify the user and the function will only run if they have at least checked one box.
    
    Tested for invalid input of no boxes checked and it works as it should. Program does not break

--- src/index.svelte
+++ src/index.svelte
@@ -1,5 +1,5 @@
 <script>
-  let lowercase = false;
+  let lowercase = true;
   let numbers = false;
   let specialCharacters = false;
   let uppercase = false;
@@ -8,25 +8,35 @@
   let password = "";
 
   function makePassword() {
-    while (password.length < passwordLength) {
+    password = "";
+    if (
+      lowercase === false &&
+      numbers === false &&
+      specialCharacters === false &&
+      uppercase === false
+    ) {
+      password = "Please choose a checkbox";
+    } else {
+      while (password.length < passwordLength) {
         let randomSelection = Math.floor(Math.random() * 4 + 1);
-      if (randomSelection === 1 && lowercase) {
-        let randomNum = Math.floor(Math.random() * 26 + 97);
-        password += String.fromCharCode(randomNum);
+        if (randomSelection === 1 && lowercase) {
+          let randomNum = Math.floor(Math.random() * 26 + 97);
+          password += String.fromCharCode(randomNum);
+        }
+        if (randomSelection === 2 && numbers) {
+          let randomNum = Math.floor(Math.random() * 10 + 48);
+          password += String.fromCharCode(randomNum);
+        }
+        if (randomSelection === 3 && specialCharacters) {
+          let randomNum = Math.floor(Math.random() * 11 + 33);
+          password += String.fromCharCode(randomNum);
+        }
+        if (randomSelection === 4 && uppercase) {
+          let randomNum = Math.floor(Math.random() * 26 + 65);
+          password += String.fromCharCode(randomNum);
+        }
       }
-      if (randomSelection === 2 && numbers) {
-        let randomNum = Math.floor(Math.random() * 10 + 48);
-        password += String.fromCharCode(randomNum);
-      }
-      if (randomSelection === 3 && specialCharacters) {
-        let randomNum = Math.floor(Math.random() * 11 + 33);
-        password += String.fromCharCode(randomNum);
-      }
-      if (randomSelection === 4 && uppercase) {
-        let randomNum = Math.floor(Math.random() * 26 + 65);
-        password += String.fromCharCode(randomNum);
-      }
-    } 
+    }
   }
 </script>
 
@@ -45,13 +55,13 @@
     bind:value={passwordLength} />
 
   <p>I would like:</p>
-  <input type="checkbox" bind:value={lowercase} />
+  <input type="checkbox" bind:checked={lowercase} />
   Lowercase Letters
-  <input type="checkbox" bind:value={numbers} />
+  <input type="checkbox" bind:checked={numbers} />
   Numbers
-  <input type="checkbox" bind:value={specialCharacters} />
+  <input type="checkbox" bind:checked={specialCharacters} />
   Special Characters
-  <input type="checkbox" bind:value={uppercase} />
+  <input type="checkbox" bind:checked={uppercase} />
   Uppercase Letters
   <button on:click={makePassword}>Generate Password!</button>
   {password}
