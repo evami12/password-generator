973d1f19c34d3db4a0d8392ed9f0a01e5bb6e740
Evelyn <kuo.e@rangiruru.school.nz>
2020-05-27 12:02:31 +1200

    generate password button disappears if no boxes chosen
    the generate button disappears if the user doesn't select a box, and it prompts them to choose at least one box.
    
    Tested for inputs of no boxes and one box or all boxes, works as it should

--- src/index.svelte
+++ src/index.svelte
@@ -6,35 +6,28 @@
 
   let passwordLength = 8;
   let password = "";
+  let passwordItems = "";
 
   function makePassword() {
     password = "";
-    if (
-      lowercase === false &&
-      numbers === false &&
-      specialCharacters === false &&
-      uppercase === false
-    ) {
-      password = "Please tick at least one checkbox";
-    } else {
-      while (password.length < passwordLength) {
-        let randomSelection = Math.floor(Math.random() * 4 + 1);
-        if (randomSelection === 1 && lowercase) {
-          let randomNum = Math.floor(Math.random() * 26 + 97);
-          password += String.fromCharCode(randomNum);
-        }
-        if (randomSelection === 2 && numbers) {
-          let randomNum = Math.floor(Math.random() * 10 + 48);
-          password += String.fromCharCode(randomNum);
-        }
-        if (randomSelection === 3 && specialCharacters) {
-          let randomNum = Math.floor(Math.random() * 11 + 33);
-          password += String.fromCharCode(randomNum);
-        }
-        if (randomSelection === 4 && uppercase) {
-          let randomNum = Math.floor(Math.random() * 26 + 65);
-          password += String.fromCharCode(randomNum);
-        }
+
+    while (password.length < passwordLength) {
+      let randomSelection = Math.floor(Math.random() * 4 + 1);
+      if (randomSelection === 1 && lowercase) {
+        let randomNum = Math.floor(Math.random() * 26 + 97);
+        password += String.fromCharCode(randomNum);
+      }
+      if (randomSelection === 2 && numbers) {
+        let randomNum = Math.floor(Math.random() * 10 + 48);
+        password += String.fromCharCode(randomNum);
+      }
+      if (randomSelection === 3 && specialCharacters) {
+        let randomNum = Math.floor(Math.random() * 11 + 33);
+        password += String.fromCharCode(randomNum);
+      }
+      if (randomSelection === 4 && uppercase) {
+        let randomNum = Math.floor(Math.random() * 26 + 65);
+        password += String.fromCharCode(randomNum);
       }
     }
   }
@@ -55,7 +48,7 @@
     bind:value={passwordLength} />
 
   <p>I would like:</p>
-  <input type="checkbox" bind:checked={lowercase} />
+  <input type="checkbox" bind:checked={lowercase} group={passwordItems} />
   Lowercase Letters
   <input type="checkbox" bind:checked={numbers} />
   Numbers
@@ -63,11 +56,16 @@
   Special Characters
   <input type="checkbox" bind:checked={uppercase} />
   Uppercase Letters
-  <button on:click={makePassword}>Generate Password!</button>
-  {password}
 
-{#if passwordLength < 6}
-<p>Password should be at least 6 letters</p>
-{/if}
+  
+  {#if lowercase === false && numbers === false && specialCharacters === false && uppercase === false}
+    <p>Please tick a box</p>
+  {:else}
+    <button on:click={makePassword}>Generate Password!</button>
+  {/if}
+  {password}
+  {#if passwordLength < 6}
+    <p>Password should be at least 6 letters</p>
+  {/if}
 
 </section>
