b36f7afb616e5496fe2520e1a7a99a17d83f3258
Evelyn <kuo.e@rangiruru.school.nz>
2020-06-02 10:26:22 +1200

    added css

--- src/index.svelte
+++ src/index.svelte
@@ -1,21 +1,20 @@
 <script>
-//Creating variables for different character types & numbers
+  //Creating variables for different character types & numbers
   let lowercase = true;
   let numbers = false;
   let specialCharacters = false;
   let uppercase = false;
-
 
   //Setting default password length to 8
   let passwordLength = 8;
   let password = "";
 
   function makePassword() {
-    console.log('Generate Password button is clicked')
+    console.log("Generate Password button is clicked");
     password = "";
 
     while (password.length < passwordLength) {
-      console.log('Generating password loop started')
+      console.log("Generating password loop started");
 
       //Randomly selecting which type of character to choose
       let randomSelection = Math.floor(Math.random() * 4 + 1);
@@ -41,7 +40,21 @@
 </script>
 
 <style>
+  #checkboxes {
+    display: block;
+    padding: 10px;
+    margin-left: 10px;
+    margin-right: 20px;
+    margin-bottom: 10px;
+    background-color: antiquewhite;
+    color: black;
+  }
 
+  #password {
+    display: block;
+    size: 5px;
+    margin-top: 20px;
+  }
 </style>
 
 <section class="section content">
@@ -56,40 +69,42 @@
 
   <p>I would like:</p>
 
-<label>
-  <input type="checkbox" bind:checked={lowercase} />
-  Lowercase Letters
-</label>  
+  <div id="checkboxes">
+    <label>
+      <input type="checkbox" bind:checked={lowercase} />
+      Lowercase Letters
+    </label>
 
-<label>
-<input type="checkbox" bind:checked={numbers} />
-  Numbers
-</label>  
+    <label>
+      <input type="checkbox" bind:checked={numbers} />
+      Numbers
+    </label>
 
-<label>
-<input type="checkbox" bind:checked={specialCharacters} />
-  Special Characters
-</label>  
+    <label>
+      <input type="checkbox" bind:checked={specialCharacters} />
+      Special Characters
+    </label>
 
-<label>
-<input type="checkbox" bind:checked={uppercase} />
-  Uppercase Letters
-</label>
+    <label>
+      <input type="checkbox" bind:checked={uppercase} />
+      Uppercase Letters
+    </label>
+  </div>
 
+  <label>
+    <!--If No checkboxes are selected, the button will disappear and it will prompt input from user-->
+    {#if lowercase === false && numbers === false && specialCharacters === false && uppercase === false}
+      <p>Please select a checkbox</p>
+    {:else}
+      <button class="button is-success" on:click={makePassword}>
+        Generate Password!
+      </button>
+    {/if}
+  </label>
 
-<label>
-<!--If No checkboxes are selected, the button will disappear and it will prompt input from user-->
-  {#if lowercase === false && numbers === false && specialCharacters === false && uppercase === false}
-    <p>Please select a checkbox</p>
-  {:else}
-    <button class="button is-success" on:click={makePassword}>Generate Password!</button>
-  {/if}
-</label>
+  <p id="password">Password: {password}</p>
 
- {password}
-
-
-<!--Suggests a password length of at least six if user selects number less than six-->
+  <!--Suggests a password length of at least six if user selects number less than six-->
   {#if passwordLength < 6}
     <p>Password should be at least 6 letters</p>
   {/if}
